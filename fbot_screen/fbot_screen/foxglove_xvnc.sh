20edf8f26058f1d9850130601193d1fe86a4f119
vitoranello <vitoranello@gmail.com>
2026-06-12 09:27:35 -0300

    Rebase codes to work with the new tablet (A9 +)

--- fbot_screen/fbot_screen/foxglove_xvnc.sh
+++ fbot_screen/fbot_screen/foxglove_xvnc.sh
@@ -1,15 +1,18 @@
 #! /bin/bash
 foxglove-studio &
 sleep 5
-WINDOW_ID=$(xwininfo -name "Dashboard | Foxglove" | grep "Window id:" | awk '{print $4}')
-WINDOW_WIDTH=$(xwininfo -name "Dashboard | Foxglove" | grep "Width:" | awk '{print $2}')
-WINDOW_HEIGHT=$(xwininfo -name "Dashboard | Foxglove" | grep "Height:" | awk '{print $2}')
 
+WINDOW_ID=$(xwininfo -name "Foxglove" | grep "Window id:" | awk '{print $4}')
 echo "Window ID: $WINDOW_ID"
 
-CLIP=$WINDOW_WIDTH'x'$WINDOW_HEIGHT
+xdotool windowsize $WINDOW_ID 1920 1080
+sleep 1
 
+CROP_WIDTH=1920
+CROP_HEIGHT=870
+OFFSET_X=0
+OFFSET_Y=140
 
+CLIP="${CROP_WIDTH}x${CROP_HEIGHT}+${OFFSET_X}+${OFFSET_Y}"
 
-x11vnc -id $WINDOW_ID -forever -noremote -noxdamage -clip  $CLIP+0+80
-
+x11vnc -id $WINDOW_ID -forever -noremote -noxdamage -clip  $CLIP -speeds -defer -scale 1920x1200 -viewonly
