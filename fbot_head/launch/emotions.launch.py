9295e8b4ea33cc47ea555ec33603e741c9a55665
maninhofurg <maninhofurg@gmail.com>
2026-08-24 18:25:20 -0300

    feat(launch): add default_blink argument and translate comments to English

--- fbot_head/launch/emotions.launch.py
+++ fbot_head/launch/emotions.launch.py
@@ -4,6 +4,10 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
 
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+
+
 def generate_launch_description():
 
     motors_config = os.path.join(
@@ -12,21 +16,20 @@ def generate_launch_description():
         'motors.yaml'
         )
     
-    # emotions_config = os.path.join(
-    #     get_package_share_directory('fbot_head'),
-    #     'config',
-    #     'emotions.yaml'
-    #     )
+    default_blink_arg = DeclareLaunchArgument(
+        'default_blink',
+        default_value='False',
+        description='Defines whether the robot should start with blinking enabled (True) or disabled (False).'
+    )
+    
+    default_blink_config = LaunchConfiguration('default_blink')
 
-        
     return launch.LaunchDescription([
+        default_blink_arg,
         Node(
             package='fbot_head',
             executable='emotions_bridge',
             name='emotions_bridge',
-            parameters = [motors_config]), #emotions_config]),
-        # Node(
-        #     package='fbot_head',
-        #     executable='emotions_publisher',
-        #     name='emotions_publisher')
-    ])
+
+            parameters = [motors_config, {'default_blink': default_blink_config}]),
+
